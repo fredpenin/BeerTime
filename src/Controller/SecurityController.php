3b9e81d90aa250ed6127e8e961a1a694c389d96b
fredpenin <fred.penin@gmail.com>
2018-12-07 09:47:35 +0100

    add sessions

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,19 +30,24 @@ class SecurityController extends AbstractController
 
             $om->persist($user);
             $om->flush();
-        }
 
-        return $this->redirectToRoute('security_login');
-            //           render('security\registration.html.twig',[
-            // 'form' => $form->createView()
-            // ]);
+            return $this->redirectToRoute('security_login');
+        } 
+        return $this->render('security\registration.html.twig',[
+        'form' => $form->createView()
+        ]);
+
+
     }
     /**
      * @Route("/connexion", name="security_login")
      */
     public function login(){
+        //sait qu'il doit connecter l'utilisateur grace au login_path/check_path qui pointent sur 'security_login' dans security.yaml, 
+        //  et fait le lien grace au '_username' et '_password' ds le formulaire
         return $this->render('security/login.html.twig');
     }
+
 
     /**
      * @Route("/deconnexion", name="security_logout")
